26ebb1371911396dd5a98671dacd6a983bc5694e
yl <1535523313@qq.com>
2022-03-09 15:54:54 +0800

    工程模块化

---
+++ M_ReplicatorDemo/msg/src/main/java/org/example/msg/CommonReqMsg.java
@@ -0,0 +1,61 @@
+package org.example.msg;
+
+import jnr.ffi.annotations.In;
+
+import java.io.Serializable;
+
+/**
+ * @description: TODO
+ * @author: xiaoliang
+ * @date: 2022/3/9 15:34
+ **/
+public class CommonReqMsg implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -3169263166233919467L;
+    private String from;
+    private String msgId;
+    private Integer event;
+
+    public CommonReqMsg(){
+
+    }
+
+    public CommonReqMsg(String from,String msgId, Integer event) {
+        this.event = event;
+        this.from = from;
+        if (event == null) {
+            this.msgId = this.from+"_"+this.getClass().getSimpleName() + msgId;
+        } else {
+            this.msgId = this.from+"_"+event + msgId;
+        }
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(String msgId) {
+        this.msgId = msgId;
+    }
+
+    public Integer getEvent() {
+        return event;
+    }
+
+    public void setEvent(Integer event) {
+        this.event = event;
+    }
+
+
+}
