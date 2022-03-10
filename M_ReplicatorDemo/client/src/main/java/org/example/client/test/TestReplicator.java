3601409ec51318053e4be9d1ae5dff35b4f3c921
yl <1535523313@qq.com>
2022-03-10 16:03:49 +0800

    replicator是持久化的磁盘的，重启集群后，还可以查到之前缓存的数据

--- M_ReplicatorDemo/client/src/main/java/org/example/client/test/TestReplicator.java
+++ M_ReplicatorDemo/client/src/main/java/org/example/client/test/TestReplicator.java
@@ -13,17 +13,22 @@ import org.example.msg.Student;
  **/
 public class TestReplicator {
     public static void main(String[] args) {
-        for (int i = 0; i < 1; i++) {
-            Student student = new Student();
-            student.setMsgId("1234987");
-            student.setId(139);
-            student.setName("liang");
-            AKKAClient.getInstance().send("/user/helloWorld",student);
-        }
+//        for (int i = 0; i < 1; i++) {
+//            Student student = new Student();
+//            student.setMsgId("1234987");
+//            student.setId(139);
+//            student.setName("liang");
+//            AKKAClient.getInstance().send("/user/helloWorld",student);
+//        }
         //获取
         GetStudent student = new GetStudent();
         student.setMsgId("100000111");
         student.setId(139);
+        AKKAClient.getInstance().send("/user/helloWorld",student);
+
+        student = new GetStudent();
+        student.setMsgId("100000222");
+        student.setId(140);
         AKKAClient.getInstance().send("/user/helloWorld",student);
     }
 }
